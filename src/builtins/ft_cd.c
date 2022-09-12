002eb6a1f12caaf4320aceec648df4c3e365fc14
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2022-09-12 14:56:08 +0200

    cd nuts

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: buiterma <buiterma@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/08 13:34:33 by buiterma      #+#    #+#                 */
-/*   Updated: 2022/09/09 19:56:41 by buiterma      ########   odam.nl         */
+/*   Updated: 2022/09/12 14:52:11 by buiterma      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,61 +15,67 @@
 #include "../../libs/libft/include/libft.h"
 #include <stdio.h>
 
-static char	*prev_dir(char *cur_dir)
+static void	set_pwd(char *old_dir)
 {
-	int		len;
-	char	*prev_dir;
-
-	len = ft_strlen(cur_dir);
-	if (ft_strncmp(cur_dir, "/", 1) == 0 && ft_strlen(cur_dir) == 1)
-		return (cur_dir);
-	while (len > 0)
-	{
-		if (cur_dir[len] == '/')
-			break ;
-		len--;
-	}
-	prev_dir = ft_substr(cur_dir, 0, len);
-	if (!prev_dir)
-		return (NULL);
-	return (prev_dir);
+	cur_dir = getcwd(NULL, -1);
+	g_shell.env = get_env(g_shell.env, "OLDPWD");
+	free (g_shell.env->value);
+	g_shell.env->value = old_dir;
+	g_shell.env = get_env(g_shell.env, "PWD");
+	free (g_shell.env->value);
+	g_shell.env->value = cur_dir;
+	free(cur_dir);
 }
 
-// Set PWD and OLDPWD to correct values after performing CD.
-// Clean up function for readability.
-int	ft_cd(int argc, const char **argv)
+static bool	set_dir(const char *path)
 {
-	char	*temp_dir;
 	char	*cur_dir;
 
 	cur_dir = getcwd(NULL, -1);
-	if (argc < 2)
+	if (chdir(path) < 0)
+	{
+		ft_putstr_fd(path, STDERR_FILENO);
+		ft_putendl_fd(": No such file or directory.", STDERR_FILENO);
+		free(cur_dir);
+		return (FALSE);
+	}
+	if (!set_pwd(cur_dir))
+		return (FALSE);
+	return (TRUE);
+}
+
+int	ft_cd(int argc, const char **argv)
+{
+	if (argc > 2)
+	{
+		ft_putendl_fd("Too many arguments.", STDERR_FILENO);
 		return (1);
-	if (ft_strncmp(argv[1], "..", 2) == 0 || ft_strncmp(argv[1], "../", 3) == 0)
-		chdir(prev_dir(cur_dir));
-	// else
-	// {
-	// 	cur_dir = ft_strjoin(temp_dir, "/");
-	// 	cur_dir = ft_strappend(cur_dir, argv[1]);
-	// 	if (chdir(cur_dir) == 0)
-	// 	{
-	// 		ft_putstr_fd(cur_dir, STDERR_FILENO);
-	// 		ft_putendl_fd(": No such file or directory.", STDERR_FILENO);
-	// 	}
-	// }
+	}
+	if (argc == 1 || ft_strncmp(cur_dir, "/", 1) == 0)
+	{
+		g_shell.env = get_env(g_shell.env, "HOME");
+		if (!set_dir(g_shell.env->value))
+			return (1);
+	}
+	else
+	{
+		if (!set_dir(argv[1]))
+			return (1);
+	}
 	return (0);
 }
 
 int	main(int argc, char const *argv[], char **envp)
 {
-	char	*cur_dir;
-	t_env	*pwd;
-	t_env	*old_pwd;
+	// char	*cur_dir;
+	// t_env	*pwd;
+	// t_env	*old_pwd;
 
-	parse_environment(envp);
-	pwd = get_env(g_shell.env, "PWD");
-	old_pwd = get_env(g_shell.env, "OLDPWD");
-	printf("pwd: %s\nold pwd: %s\n", pwd->value, old_pwd->value);
-	ft_cd(argc + 1, &argv[1]);
+	// parse_environment(envp);
+	// pwd = get_env(g_shell.env, "PWD");
+	// old_pwd = get_env(g_shell.env, "OLDPWD");
+	// printf(BOLD GREEN"pwd: %s\nold pwd: %s\n", pwd->value, old_pwd->value);
+	// ft_cd(argc + 1, &argv[1]);
+	printf("%d\n",chdir(".."));
 	return (0);
 }
