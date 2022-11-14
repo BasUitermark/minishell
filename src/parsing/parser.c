4c9f4b1cd4b0334265a68b300c132b25b5fae503
w1codam <jde-groo@student.codam.nl>
2022-11-14 12:29:56 +0100

    leak fix

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: buiterma <buiterma@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/24 11:29:47 by buiterma      #+#    #+#                 */
-/*   Updated: 2022/11/03 12:39:33 by jde-groo      ########   odam.nl         */
+/*   Updated: 2022/11/14 12:29:10 by jde-groo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,12 @@ static int	parts(char *cmd)
 	return (parts);
 }
 
+static void	free_two_things_because_norm(void *one, void *two)
+{
+	free(one);
+	free(two);
+}
+
 static bool	redo_args(t_command *cmd)
 {
 	int		index[2];
@@ -48,7 +54,7 @@ static bool	redo_args(t_command *cmd)
 		split_newargs[1][index[0]] = split_newargs[0][index[0]];
 		index[0]++;
 	}
-	free(split_newargs[0]);
+	free_two_things_because_norm(cmd->args[0], split_newargs[0]);
 	while (cmd->args[1 + index[1]])
 	{
 		split_newargs[1][index[0] + index[1]] = cmd->args[1 + index[1]];
